466af428dbf71c61d91bef6bca1dea3e7952f31a
JON <jalvarez-2023415@kinal.edu.gt>
2025-06-20 02:53:00 -0600

    in-screen

--- App.js
+++ App.js
@@ -1,76 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from 'react-native-vector-icons/Ionicons'
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-    </View>
-  )
-}
-
-function NewScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>NewScreen</Text>
-    </View>
-  )
-}
-
-function SettingsScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>SettingsScreen</Text>
-    </View>
-  )
-}
-
-
-
-const Tab = createBottomTabNavigator();
+import MainStackNavigator from "./navigation/MainNavigatior";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'home'
-                : 'home-outline';
-            } else if (route.name === 'New') {
-              iconName = focused ? 'car' : 'car-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'settings' : 'settings-outline';
-            }
-
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        {/* Tus pantallas van aquí, por ejemplo: */}
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="New" component={NewScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <MainStackNavigator />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
